9aea20b8e14b04826a9af9bafabc211015dddd77
AafaqAhmed <afaqahmad6296@gmail.com>
2022-01-12 21:11:58 +0500

    Added a Indendent TextField

---
+++ Sources/AATools/LeftIndentTextField.swift
@@ -0,0 +1,32 @@
+//
+//  LeftIndentTextField.swift
+//  
+//
+//  Created by Aafaq on 12/01/2022.
+//
+
+import UIKit
+
+class LeftIndentTextField: UITextField {
+    
+    init(leftSpacing: CGFloat = 12, color: UIColor = .white) {
+        super.init(frame: .zero)
+        
+        let leftView = UIView()
+        leftView.setDimensions(height: 12, width: 12)
+        leftView.backgroundColor = color
+        self.leftView = leftView
+        self.backgroundColor = color
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
