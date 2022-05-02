382e4c50abe44cb6a607dc1f52539dcd55e2c5e5
AafaqAhmed <afaqahmad6296@gmail.com>
2022-05-02 12:53:00 +0500

    Added * method to String+Overloading

--- Sources/AATools/OperatorOverloading/String+Overloading.swift
+++ Sources/AATools/OperatorOverloading/String+Overloading.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 
-///  Use to multiply string realy quikc for dummy string creation
-/// - Returns: It will return the actual string by the number of times with integer you are multiplying
-func * (lhs: String, rhs: Int) -> String {
-    var string = ""
-    (0..<rhs).forEach { (_) in
-        string = string + lhs
+extension String {
+    ///  Use to multiply string realy quikc for dummy string creation
+    /// - Returns: It will return the actual string by the number of times with integer you are multiplying
+    public static func * (lhs: String, rhs: Int) -> String {
+        var string = ""
+        (0..<rhs).forEach { (_) in
+            string = string + lhs
+        }
+        return string
     }
-    return string
 }
+
