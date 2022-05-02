9ec468ca38ca5ae652d09bc98e91c9e08e4d5ab6
AafaqAhmed <afaqahmad6296@gmail.com>
2022-05-02 12:47:20 +0500

    Remove overloaded operator from CGRect+Extension

--- Sources/AATools/Extensions/CGRect+Extension.swift
+++ Sources/AATools/Extensions/CGRect+Extension.swift
@@ -8,7 +8,4 @@
 import UIKit
 
 extension CGRect {
-    public static func + (lhs: CGRect, rhs: CGRect) -> CGRect {
-        return CGRect(x: lhs.minX + rhs.minX, y: lhs.minY + rhs.minY, width: rhs.width + lhs.width, height: lhs.height + rhs.height)
-    }
 }
