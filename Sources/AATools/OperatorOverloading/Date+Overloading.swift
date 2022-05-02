fd62cbab32cd4f3205bea6b52c9fa07e24f4d6ed
AafaqAhmed <afaqahmad6296@gmail.com>
2022-05-02 12:51:54 +0500

    Overloaded === operator in Date+Overloading

--- Sources/AATools/OperatorOverloading/Date+Overloading.swift
+++ Sources/AATools/OperatorOverloading/Date+Overloading.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
-/// Use to compare string base on just date not time.
-/// - Returns: `Boolean`
-func === (lhs: Date, rhs: Date) -> Bool {
-    let formatter = DateFormatter()
-    formatter.locale = .current
-    formatter.dateFormat = "dd MM yyyy"
-    return formatter.string(from: lhs) == formatter.string(from: rhs)
+extension Date {
+    /// Use to compare string base on just date not time.
+    /// - Returns: `Boolean`
+    public static func === (lhs: Date, rhs: Date) -> Bool {
+        let formatter = DateFormatter()
+        formatter.locale = .current
+        formatter.dateFormat = "dd MM yyyy"
+        return formatter.string(from: lhs) == formatter.string(from: rhs)
+    }
 }
+
