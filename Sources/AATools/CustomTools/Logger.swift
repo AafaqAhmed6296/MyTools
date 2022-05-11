90947b05829ccf8d630b8d6673a9dba43389261c
AafaqAhmed <afaqahmad6296@gmail.com>
2022-05-11 08:38:09 +0500

    Changed the name of variable parameter to values in Debug.log

--- Sources/AATools/CustomTools/Logger.swift
+++ Sources/AATools/CustomTools/Logger.swift
@@ -5,11 +5,45 @@
 
 
 public struct Debug {
-    public static func log(message: String, variable: Any?) {
-        if variable == nil {
+    public static func log(message: String, values: Any? ) {
+        
+        if let variable = values {
+            print("MyDEBUG, \(message): \(variable)")
+        } else {
+            print("MyDEBUG, \(message)")
+        }
+
+    }
+    
+    @available(*, deprecated, message: "variable parameter is changed to value, use value i", renamed: "log(message:values:)")
+    public static func log(message: String, variable: Any? ) {
+        
+        if let variable = variable {
+            print("MyDEBUG, \(message): \(variable)")
+        } else {
+            print("MyDEBUG, \(message)")
+        }
+
+    }
+    
+    public static func log(message: String, values: Any...) {
+        if values.isEmpty  {
             print("MyDEBUG, \(message)")
         }else{
-            print("MyDEBUG, \(message):", variable!)
+            var string = "MyDEBUG, \(message): "
+            let count = values.count - 1
+            var i = 0
+            for value in values {
+                
+                if count == i {
+                    string += "\(value)"
+                } else {
+                    string += "\(value), "
+                }
+                
+                i += 1
+            }
+            print(string)
         }
     }
 }
